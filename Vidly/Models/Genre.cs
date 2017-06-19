31742f8d2f6ea0ee08ae8e64549948a123dd8be4
Bradley C <bpince@hotmail.com>
2017-06-19 22:51:14 +1000

    MIGRATION: Added Genre table and Movie table.

--- Vidly/Models/Genre.cs
+++ Vidly/Models/Genre.cs
@@ -7,21 +7,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vidly.Models
 {
-    public class Movie
+    public class Genre
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
-        public DateTime? ReleaseDate { get; set; }
-
-        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime DateAdded { get; set; }
-
-        public int AvailableAmount { get; set; }
-
-        public Genre Genre { get; set; }
     }
 }
