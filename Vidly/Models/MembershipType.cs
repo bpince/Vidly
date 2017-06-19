08fe15520caa7ba6e90ec80d3fca332a7693d58f
Bradley C <bpince@hotmail.com>
2017-06-19 20:06:42 +1000

    Add MembershipType Name property and Migration.

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -11,5 +11,6 @@ namespace Vidly.Models
         public short SignUpfee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+        public string MemberShipTypeName { get; set; }
     }
 }
