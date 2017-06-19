fa87b909f5cdc82436732a586e65c166f54573f6
Bradley <Bradley@BRADCHAKOS-PC>
2017-06-19 19:45:26 +1000

    Add project files.

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+        
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var selectedCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (selectedCustomer != null)
+            {
+                return View(selectedCustomer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            
+        }
+    }
+}
