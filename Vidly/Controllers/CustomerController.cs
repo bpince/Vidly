64a3cefd0caf758cf07fbeb75ac98c25de90e6f3
Bradley C <bpince@hotmail.com>
2017-07-20 21:48:37 +1000

    Populated Genre Table, added some customer views and logic.

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -28,6 +29,17 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MemberShipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MemberShipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
         public ActionResult Details(int? id)
         {
             var selectedCustomer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
@@ -41,6 +53,48 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             }
             
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                //one approach - TryUpdateModel(customer);
+
+                //other safer approach
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribeToNewsletter = customer.IsSubscribeToNewsletter;
+            }
+            
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customer");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MemberShipTypes = _context.MemberShipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
