63e0e3bcd5184a9c48537a3d4daf17a2071eab5b
Bradley C <bpince@hotmail.com>
2017-08-22 20:45:47 +1000

    Section 7 - Datatable, etc.

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -23,10 +23,7 @@ namespace Vidly.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-
-            return View(customers);
+            return View();
         }
 
         public ActionResult New()
